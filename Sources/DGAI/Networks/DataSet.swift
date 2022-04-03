7e1468516584329606d1a828d6ca65aec8b014ea
Dmitry Grusha <Dima-fx@ukr.net>
2022-04-03 17:55:31 +0300

    [*] upd dataSet

--- Sources/DGAI/Networks/DataSet.swift
+++ Sources/DGAI/Networks/DataSet.swift
@@ -1,4 +1,9 @@
-public struct DataSet {
+public class DataSet {
   public let input: [Float]
   public let expected: Float
+  
+  public init(input: [Float], expected: Float) {
+    self.input = input
+    self.expected = expected
+  }
 }
